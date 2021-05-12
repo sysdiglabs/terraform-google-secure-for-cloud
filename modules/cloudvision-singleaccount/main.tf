ffbd7eca8fa45dcab7372b81b2fe8399ece22971
Álvaro Iradier <alvaro.iradier@sysdig.com>
2021-05-12 19:47:17 +0200

    feat: Add CloudScanning (#1)
    * Add CloudScanning
    * Change some variable naming to match the CFTs
    * Some refactoring
    * Add GCP_LOCATION to CloudScanning
    * Add CloudBuild editor role

--- modules/cloudvision-singleaccount/main.tf
+++ modules/cloudvision-singleaccount/main.tf
@@ -1,22 +1,52 @@
+provider "google" {
+  project = var.project_name
+}
+
+locals {
+  deploy_cloudconnector = var.cloudconnector_deploy
+  deploy_cloudscanning  = var.gcr_image_scanning_deploy || var.cloudrun_image_scanning_deploy
+  deploy_cloudbench     = var.cloudbench_deploy
+  verify_ssl            = length(regexall("^https://.*?\\.sysdig.com/?", var.sysdig_secure_endpoint)) != 0
+  bucket_name           = "${substr(lower(var.naming_prefix), 0, 29)}-config"
+}
+
 module "cloud-connector" {
+  count = local.deploy_cloudconnector ? 1 : 0
+
   source = "../cloud-connector"
 
-  name               = "${var.name}-cloud-connector"
-  location           = var.location
-  bucket_config_name = var.name
-  project_name       = var.project_name
-  secure_api_token   = var.secure_api_token
-  verify_ssl         = var.verify_ssl
-  config_content     = <<EOF
+  naming_prefix           = var.naming_prefix
+  location                = var.location
+  bucket_config_name      = local.bucket_name
+  sysdig_secure_api_token = var.sysdig_secure_api_token
+  sysdig_secure_endpoint  = var.sysdig_secure_endpoint
+  verify_ssl              = local.verify_ssl
+  config_content          = <<EOF
 rules:
   - secure:
-      url: ${var.secure_api_url}
+      url: ${var.sysdig_secure_endpoint}
 ingestors:
   - auditlog:
       project: ${var.project_name}
-      interval: 10m
+      interval: 30s
 notifiers:
   - secure:
-      url: ${var.secure_api_url}
+      url: ${var.sysdig_secure_endpoint}
   EOF
+}
+
+module "cloud-scanning" {
+  count = local.deploy_cloudscanning ? 1 : 0
+
+  source = "../cloud-scanning"
+
+  naming_prefix           = var.naming_prefix
+  project_name            = var.project_name
+  location                = var.location
+  sysdig_secure_api_token = var.sysdig_secure_api_token
+  sysdig_secure_endpoint  = var.sysdig_secure_endpoint
+
+  verify_ssl      = local.verify_ssl
+  deploy_gcr      = var.gcr_image_scanning_deploy
+  deploy_cloudrun = var.cloudrun_image_scanning_deploy
 }
--- modules/cloudvision-singleaccount/variables.tf
+++ modules/cloudvision-singleaccount/variables.tf
@@ -1,24 +1,51 @@
-variable "name" {
+variable "naming_prefix" {
   type        = string
-  description = "Deployment Name"
+  default     = "SysdigCloud"
+  description = "Prefix for resource names. Use the default unless you need to install multiple instances, and modify the deployment at the main account accordingly"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9\\-]+$", var.naming_prefix)) && length(var.naming_prefix) > 1 && length(var.naming_prefix) <= 64
+    error_message = "Must enter a naming prefix up to 64 alphanumeric characters."
+  }
 }
+
 variable "project_name" {
   type        = string
   description = "Project name of the Google Cloud Platform"
 }
-variable "secure_api_token" {
-  type        = string
-  description = "Sysdig's Secure API Token"
-}
-variable "secure_api_url" {
-  type        = string
-  description = "Sysdig's Secure API URL"
-}
-variable "verify_ssl" {
-  type        = bool
-  description = "Verify Sysdig's Secure SSL"
-}
+
 variable "location" {
   type        = string
   description = "Zone where the stack will be deployed"
+}
+
+variable "cloudconnector_deploy" {
+  type    = bool
+  default = true
+}
+
+variable "cloudbench_deploy" {
+  type    = bool
+  default = true
+}
+
+variable "gcr_image_scanning_deploy" {
+  type    = bool
+  default = true
+}
+
+variable "cloudrun_image_scanning_deploy" {
+  type    = bool
+  default = true
+}
+
+variable "sysdig_secure_api_token" {
+  type        = string
+  description = "Sysdig's Secure API Token"
+}
+
+variable "sysdig_secure_endpoint" {
+  type        = string
+  default     = "https://secure.sysdig.com"
+  description = "Sysdig Secure API endpoint"
 }
