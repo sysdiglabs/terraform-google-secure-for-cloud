0bf2316ea34339d9ab3d74821bf4fe452e89039b
Néstor Salceda <nestor.salceda@sysdig.com>
2021-06-14 12:45:53 +0200

    refactor: Simplify prefixes

--- main.tf
+++ main.tf
@@ -1,8 +1,6 @@
 locals {
   deploy_cloudconnector = var.cloudconnector_deploy
   verify_ssl            = length(regexall("^https://.*?\\.sysdig.com/?", var.sysdig_secure_endpoint)) != 0
-  bucket_name           = "${substr(lower(var.naming_prefix), 0, 29)}-config"
-  naming_prefix         = var.naming_prefix == null ? data.google_project.project.project_id : var.naming_prefix
 }
 
 data "google_project" "project" {
@@ -13,9 +11,7 @@ module "cloud_connector" {
 
   source = "./modules/cloud-connector"
 
-  naming_prefix           = local.naming_prefix
   location                = var.location
-  bucket_config_name      = local.bucket_name
   sysdig_secure_api_token = var.sysdig_secure_api_token
   sysdig_secure_endpoint  = var.sysdig_secure_endpoint
   verify_ssl              = local.verify_ssl
--- variables.tf
+++ variables.tf
@@ -1,12 +1,7 @@
 variable "naming_prefix" {
   type        = string
-  default     = null
+  default     = ""
   description = "Prefix for resource names. Use the default unless you need to install multiple instances, and modify the deployment at the main account accordingly"
-
-  validation {
-    condition     = can(regex("^[a-zA-Z0-9\\-]+$", var.naming_prefix)) && length(var.naming_prefix) > 1 && length(var.naming_prefix) <= 64
-    error_message = "Must enter a naming prefix up to 64 alphanumeric characters."
-  }
 }
 
 variable "location" {
