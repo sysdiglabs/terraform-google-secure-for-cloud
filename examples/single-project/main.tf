624e2a2e68949122560337714d2a342ee0261667
Noah Kraemer <noah.kraemer@sysdig.com>
2021-09-02 12:56:36 -0700

    use locals

--- examples/single-project/main.tf
+++ examples/single-project/main.tf
@@ -102,7 +102,7 @@ provider "google-beta" {
 provider "sysdig" {
   sysdig_secure_url          = var.sysdig_secure_endpoint
   sysdig_secure_api_token    = var.sysdig_secure_api_token
-  sysdig_secure_insecure_tls = length(regexall("https://.*?\\.sysdig(cloud)?.com/?", var.sysdig_secure_endpoint)) == 1 ? false : true
+  sysdig_secure_insecure_tls = !local.verify_ssl
 }
 
 module "cloud_bench" {
