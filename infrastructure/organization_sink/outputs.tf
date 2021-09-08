a2085c05a785968f92cf065ca36f58f7bd0f627a
Hayk Kocharyan <hayk@MacBook-Pro-de-Hayk-Work.local>
2021-09-08 09:38:08 +0200

    chore: delete duplicated folders

--- infrastructure/organization_sink/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "pubsub_topic_id" {
-  value       = google_pubsub_topic.topic.id
-  description = "Cloud Connector PubSub single account topic id"
-}
--- infrastructure/secrets/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "secure_api_token_secret_name" {
-  value       = google_secret_manager_secret.secure_api_secret.secret_id
-  description = "Sysdig's Secure API Token secret name"
-}
