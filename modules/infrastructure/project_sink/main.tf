272ce39281aac634c6f3593338319530b8193fb3
Federico Barcelona <fede_rico_94@hotmail.com>
2021-09-09 10:32:40 +0200

    refactor: Remove service variable from project sink

--- modules/infrastructure/project_sink/main.tf
+++ modules/infrastructure/project_sink/main.tf
@@ -1,9 +1,9 @@
 resource "google_pubsub_topic" "topic" {
-  name = "${var.naming_prefix}-cloud-${var.service}-topic"
+  name = "${var.naming_prefix}-topic"
 }
 
 resource "google_logging_project_sink" "project_sink" {
-  name                   = "${var.naming_prefix}-cloud-${var.service}-project-sink"
+  name                   = "${var.naming_prefix}-project-sink"
   destination            = "pubsub.googleapis.com/${google_pubsub_topic.topic.id}"
   unique_writer_identity = true
   filter                 = var.filter
