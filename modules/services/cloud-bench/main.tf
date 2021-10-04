619bbf26490e752e404d3e69d5e56efb8a7f3d6f
Alex <alex.qiu@sysdig.com>
2021-10-04 14:34:43 -0700

    fix uncomment

--- modules/services/cloud-bench/main.tf
+++ modules/services/cloud-bench/main.tf
@@ -2,18 +2,11 @@ locals {
   project_ids = var.is_organizational ? var.project_ids : [var.project_id]
 }
 
-//module "trust_relationship" {
-//  for_each = toset(local.project_ids)
-//  source   = "./trust_relationship"
-//
-//  project_id = each.key
-//  role_name  = var.role_name
-//}
-
 module "trust_relationship" {
-  source = "./trust_relationship"
+  for_each = toset(local.project_ids)
+  source   = "./trust_relationship"
 
-  project_id = var.project_id
+  project_id = each.key
   role_name  = var.role_name
 }
 
