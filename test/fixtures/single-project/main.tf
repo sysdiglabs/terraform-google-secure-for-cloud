5564addea708a2c4acf22ed60660057d98f09b84
iru <irune.prado@sysdig.com>
2021-10-04 17:44:45 +0200

    chore: pre-commit

--- test/fixtures/single-project/main.tf
+++ test/fixtures/single-project/main.tf
@@ -8,7 +8,7 @@ module "sfc_example_single_project" {
   source = "../../../examples/single-project"
 
   sysdig_secure_api_token = var.sysdig_secure_api_token
-  project_id              = var.project_id  
+  project_id              = var.project_id
   naming_prefix           = "sfc${random_string.random.result}"
   create_gcr_topic        = false
 }
