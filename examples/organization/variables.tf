5c14caa80d6d063cea98f7a07ebcf6692d06164d
Noah Kraemer <58708692+nkraemer-sysdig@users.noreply.github.com>
2021-10-06 03:38:55 -0700

    feat(bench): Cleanup naming-prefix (#53)
    * feat(bench) Cleanup naming-prefix
    
    * re-add display name
    
    * revert workload identity pool prefixing
    
    Co-authored-by: Noah Kraemer <noah.kraemer@sysdig.com>
    Co-authored-by: Hayk Kocharyan <noah.kraemer@sysdig.com>

--- examples/organization/variables.tf
+++ examples/organization/variables.tf
@@ -30,7 +30,7 @@ variable "naming_prefix" {
   default     = "sfc"
 
   validation {
-    condition     = can(regex("^[a-z0-9_]+$", var.naming_prefix))
+    condition     = can(regex("^[a-z0-9]+$", var.naming_prefix))
     error_message = "ERROR: Invalid naming_prefix. must contain only lowercase letters (a-z) and numbers (0-9)."
   }
 }
@@ -45,6 +45,12 @@ variable "benchmark_project_ids" {
   default     = []
   type        = list(string)
   description = "Google cloud project IDs to run Benchmarks on"
+}
+
+variable "benchmark_regions" {
+  type        = list(string)
+  description = "List of regions in which to run benchmarks. If empty, the task will contain all regions by default."
+  default     = []
 }
 
 variable "create_gcr_topic" {
--- modules/services/cloud-bench/variables.tf
+++ modules/services/cloud-bench/variables.tf
@@ -1,21 +1,13 @@
+variable "project_id" {
+  type        = string
+  description = "ID of project to run the benchmark on"
+}
+
 variable "regions" {
   type        = list(string)
   description = "List of regions in which to run the benchmark. If empty, the task will contain all regions by default."
   default     = []
 }
-
-variable "project_id" {
-  type        = string
-  description = "ID of project to run the benchmark on"
-  default     = ""
-}
-
-variable "role_name" {
-  type        = string
-  description = "The name of the Service Account that will be created."
-  default     = "sysdigcloudbench"
-}
-
 
 # --------------------------
 # optionals, with defaults
@@ -26,7 +18,7 @@ variable "naming_prefix" {
   default     = "sfc"
 
   validation {
-    condition     = can(regex("^[a-z0-9_]+$", var.naming_prefix))
+    condition     = can(regex("^[a-z0-9]+$", var.naming_prefix))
     error_message = "ERROR: Invalid naming_prefix. must contain only lowercase letters (a-z) and numbers (0-9)."
   }
 }
