1f661f826debd2251c638fed3da3e70ef26d1175
iru <irune.prado@sysdig.com>
2021-12-16 13:49:09 +0100

    refactor: remove scanning module (#79)

--- modules/services/cloud-scanning/pubsub_permissions.tf
+++
@@ -1,4 +0,0 @@
-resource "google_project_iam_member" "token_creator" {
-  member = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
-  role   = "roles/iam.serviceAccountTokenCreator"
-}
