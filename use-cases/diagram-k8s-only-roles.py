3b539a42b5392ed28797f58177803a1011e3ca60
David González Diez <dgonzalezdiez@gmail.com>
2022-07-21 09:46:23 +0200

    doc: diagrams on python

---
+++ use-cases/diagram-k8s-only-roles.py
@@ -0,0 +1,23 @@
+# diagrams as code vía https://diagrams.mingrammer.com
+from diagrams import Diagram, Cluster
+from diagrams.gcp.security import Iam
+
+diagram_attr = {
+    "pad": "0.25",
+}
+
+color_event = "firebrick"
+color_scanning = "dark-green"
+color_permission = "red"
+color_non_important = "gray"
+color_sysdig = "lightblue"
+
+with Diagram("Roles", graph_attr=diagram_attr, filename="diagram-k8s-only-roles", show=True, direction="TB"):
+
+    with Cluster("Role", graph_attr={"bgcolor": "lightblue"}):
+        serviceAccount = Iam("Service Account")
+        sysdigCloudBenchmarkRole = Iam("Sysdig Cloud \n Benchmark Role")
+        roleViewer = Iam("roles/viewer")
+
+        serviceAccount << sysdigCloudBenchmarkRole
+        serviceAccount << roleViewer
---
+++ use-cases/diagram-k8s-only.py
@@ -0,0 +1,41 @@
+# diagrams as code vía https://diagrams.mingrammer.com
+from diagrams.aws.general import General
+from diagrams import Cluster, Diagram, Edge
+from diagrams.gcp.analytics import PubSub
+from diagrams.gcp.security import Iam
+from diagrams.gcp.compute import GKE
+from diagrams.custom import Custom
+
+diagram_attr = {
+    "pad": "0.25",
+}
+
+color_event = "firebrick"
+color_scanning = "dark-green"
+color_permission = "red"
+color_non_important = "gray"
+color_sysdig = "lightblue"
+
+with Diagram("Sysdig Secure for Cloud\n(organization)", graph_attr=diagram_attr, filename="diagram-k8s-only", show=True, direction="TB"):
+
+    with Cluster("GCP account (sysdig)", graph_attr={"bgcolor": "lightblue"}):
+        sds = Custom("Sysdig Secure", "../resources/diag-sysdig-icon.png")
+        bench = General("Cloud Bench")
+        sds >> Edge(label="schedule on rand rand * * *") >> bench
+
+    with Cluster("GCP organization project", graph_attr={"bgcolor": "pink"}):
+        ccProjectSink = Custom("\nLog Router \n Sink", "../resources/sink.png")
+        orgBenchRole = Iam("Cloud Bench Role")
+
+    with Cluster("Secure for Cloud (children project)"):
+        ccBenchRole = Iam("Cloud Bench Role")
+        ccPubSub = PubSub("CC PubSub Topic")
+        ccOnK8s = GKE("CC on k8s")
+
+        ccProjectSink >> ccPubSub
+        ccOnK8s >> ccPubSub
+
+    ccOnK8s >> sds
+
+    ccBenchRole <<  Edge(color=color_non_important) <<  bench
+    orgBenchRole <<  Edge(color=color_non_important) <<  bench
