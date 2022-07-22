f8255ce6f65831c23744b465cfa7097fd69c925f
David González Diez <dgonzalezdiez@gmail.com>
2022-07-22 17:14:53 +0200

    doc: diagram title removed

--- use-cases/resources/diagram-org-k8s-threat-compliance-roles.py
+++ use-cases/resources/diagram-org-k8s-threat-compliance-roles.py
@@ -1,6 +1,7 @@
 # diagrams as code vía https://diagrams.mingrammer.com
 from diagrams import Diagram, Cluster
 from diagrams.gcp.security import Iam
+from diagrams.custom import Custom
 
 diagram_attr = {
     "pad": "0.25",
@@ -12,11 +13,11 @@ color_permission = "red"
 color_non_important = "gray"
 color_sysdig = "lightblue"
 
-with Diagram("Roles", graph_attr=diagram_attr, filename="diagram-org-k8s-threat-compliance-roles", show=True, direction="TB"):
+with Diagram("Role", graph_attr=diagram_attr, filename="diagram-org-k8s-threat-compliance-roles", show=True, direction="TB"):
 
-    with Cluster("Role", graph_attr={"bgcolor": "lightblue"}):
+    with Cluster("", graph_attr={"bgcolor": "lightblue"}):
         serviceAccount = Iam("Service Account")
-        sysdigCloudBenchmarkRole = Iam("Sysdig Cloud \n Benchmark Role")
+        sysdigCloudBenchmarkRole = Iam("Sysdig Cloud \n Benchmark Role \n [storage.buckets.getIamPolicy, \n bigquery.tables.list]")
         roleViewer = Iam("roles/viewer")
 
         serviceAccount << sysdigCloudBenchmarkRole
