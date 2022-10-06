9b29fc7331f6334d2c244c7dc18827726ffb5c54
David González Diez <dgonzalezdiez@gmail.com>
2022-10-06 10:17:45 +0200

    fix: terrascan compliance

--- modules/infrastructure/cloud_build_permission/main.tf
+++ modules/infrastructure/cloud_build_permission/main.tf
@@ -1,5 +1,9 @@
 # Required to execute cloud build runs with this same service account
 resource "google_project_iam_member" "service_account_user_itself" {
+  # Why: Image scanning is run from inside a container. As it needs to get the image from the registry it needs a token to get it from the registry.
+  # How to avoid security issues: As in the next implementation scanning will be run from within cloudrun which has needed permissions and won't need a token.
+  # Warning: Organization users musn't be able to impersonate as the created service account.
+  #ts:skip=AC_GCP_0006
   project = var.project_id
   role    = "roles/iam.serviceAccountUser"
   member  = "serviceAccount:${var.cloud_connector_sa_email}"
