4ed3e7f3523f495ed4f3dacdb296b5e33a0f7338
David González Diez <dgonzalezdiez@gmail.com>
2022-10-06 15:36:37 +0200

    docs: improve terrascan output and code comments for readability

--- modules/services/cloud-connector/pubsub_permissions.tf
+++ modules/services/cloud-connector/pubsub_permissions.tf
@@ -5,10 +5,11 @@ resource "google_project_iam_member" "event_receiver" {
 }
 
 resource "google_project_iam_member" "token_creator" {
+  # AC_GCP_0006
   # Why: Image scanning is run from inside a container. As it needs to get the image from the registry it needs a token to get it from the registry.
   # How to avoid security issues: As in the next implementation scanning will be run from within cloudrun which has needed permissions and won't need a token.
   # Warning: Organization users musn't be able to impersonate as the created service account.
-  #ts:skip=AC_GCP_0006
+  #ts:skip=AC_GCP_0006 Image scanning is run from inside a container. As it needs to get the image from the registry it needs a token to get it from the registry.
   project = var.project_id
   member  = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-pubsub.iam.gserviceaccount.com"
   role    = "roles/iam.serviceAccountTokenCreator"
