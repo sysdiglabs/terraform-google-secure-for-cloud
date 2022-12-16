e4dd6ab8c925fe6b923d94969a26d9608787a5ec
sameer-in <sameer.inamdar@sysdig.com>
2022-12-16 14:00:28 -0800

    adding tests

---
+++ test/fixtures/organization-workload-identity/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_version = ">= 0.15.0"
+
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.21.0"
+    }
+    random = {
+      version = ">= 3.1.0"
+    }
+    sysdig = {
+      source = "sysdiglabs/sysdig"
+    }
+  }
+}
+
+
+provider "sysdig" {
+  sysdig_secure_url       = var.sysdig_secure_endpoint
+  sysdig_secure_api_token = var.sysdig_secure_api_token
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.location
+}
+
+
+# This two "multiproject" providers are required for benchmark trust-identity activation on the organizational level
+provider "google" {
+  alias  = "multiproject"
+  region = var.region
+}
+
+provider "google-beta" {
+  alias  = "multiproject"
+  region = var.region
+}
+
+resource "random_string" "random" {
+  length  = 5
+  special = false
+  upper   = false
+}
+
+module "sfc_example_organization" {
+  providers = {
+    google.multiproject      = google.multiproject
+    google-beta.multiproject = google-beta.multiproject
+  }
+  source = "../../../examples/org-workload-identity-provider"
+
+  organization_domain = "draios.com"
+  name                = "sfc${random_string.random.result}"
+  repository_project_ids = [var.project_id]
+  deploy_scanning        = true
+  deploy_benchmark       = false
+}
+
