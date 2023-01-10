4279ce8dcbcb7c6ba58820c041e9302211cd95be
iru <irune.prado@sysdig.com>
2023-01-10 17:13:07 +0100

    refactor: avoid benchmark role collision with var.name (#132)
    * chore: add var.name to benchmark role to avoid collision
    * chore: add var.name to benchmark role to avoid collision
    * docs: clarify var.name usage
    * fix: test. tflint download
    * ci: tfdocs

--- examples/organization/main.tf
+++ examples/organization/main.tf
@@ -110,28 +110,3 @@ module "pubsub_http_subscription" {
   push_to_cloudrun   = true
   deploy_scanning    = var.deploy_scanning
 }
-
-
-#--------------------
-# benchmark
-#--------------------
-
-locals {
-  benchmark_projects_ids = length(var.benchmark_project_ids) == 0 ? [for p in data.google_projects.all_projects.projects : p.project_id] : var.benchmark_project_ids
-}
-
-module "cloud_bench" {
-  providers = {
-    google      = google.multiproject
-    google-beta = google-beta.multiproject
-  }
-
-  count  = var.deploy_benchmark ? 1 : 0
-  source = "../../modules/services/cloud-bench"
-
-  is_organizational   = true
-  organization_domain = var.organization_domain
-  role_name           = var.benchmark_role_name
-  regions             = var.benchmark_regions
-  project_ids         = local.benchmark_projects_ids
-}
