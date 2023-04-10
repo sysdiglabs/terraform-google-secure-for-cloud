dc338a5cc4f5f28447cc70aacba4c1f5d0203d89
sameer-in <sameer.inamdar@sysdig.com>
2023-04-10 11:41:08 -0700

    adding script to fetch gcp projects

---
+++ resources/fetch-gcp-projects.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Function to list projects under a folder recursively
+list_projects_recursive() {
+  local folder_id="$1"
+
+  # List projects under the current folder
+  printf " %s" $(gcloud projects list --filter="parent.id=$folder_id" --format="value(projectId)")
+
+  # List subfolders and call this function recursively
+  local subfolders=$(gcloud resource-manager folders list --folder=$folder_id --format="value(name)")
+  for subfolder in $subfolders; do
+    list_projects_recursive "$subfolder"
+  done
+}
+
+# List projects under the root organization
+org_id=""
+projectIds=()
+projectIds+=$(gcloud projects list --filter="parent.type=organization AND parent.id=$org_id" --format="value(projectId)")
+
+# List top-level folders
+folders=$(gcloud resource-manager folders list --organization=$org_id --format="value(name)")
+
+# Iterate through the top-level folders and list projects recursively
+for folder in $folders; do
+  projectIds+=$(list_projects_recursive "$folder")
+done
+
+echo $projectIds
