7e3481b73831f7588e9d7e4ea9bf3c1766e2bf77
Noah Kraemer <noah.kraemer@sysdig.com>
2023-10-11 16:31:57 -0700

    Pin google provider and terraform versions due to breaking changes

--- modules/services/cloud-bench-workload-identity/versions.tf
+++ modules/services/cloud-bench-workload-identity/versions.tf
@@ -1,18 +1,18 @@
 terraform {
-  required_version = ">= 0.15.0"
+  required_version = ">= 0.15.0, < 1.6.0"
 
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.21.0"
+      version = ">= 4.21.0, < 5.0.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = ">= 4.21.0"
+      version = ">= 4.21.0, < 5.0.0"
     }
     sysdig = {
       source  = "sysdiglabs/sysdig"
-      version = ">= 0.5.46"
+      version = ">= 0.5.21"
     }
   }
 }
